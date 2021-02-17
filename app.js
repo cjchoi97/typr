209d3918fca74f355b62d17be5894edafc6ccd0c
Chanu Choi <cjchoi97@gmail.com>
2021-02-17 17:25:04 -0500

    set up mongoose and .gitignore

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require("express");
-const app = express();
+const mongoose = require('mongoose');
 
-app.get("/", (req, res) => res.send("Hello World"));
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB Successfully'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 const port = process.env.PORT || 5000;
 
