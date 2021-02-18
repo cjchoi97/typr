1c8302cd3314b71b8491bd17f00df70749f59974
Chanu Choi <cjchoi97@gmail.com>
2021-02-18 18:37:47 -0500

    fix bug wth gitignore and bug with module exports

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require('express');
 const users = require('./routes/api/users');
 
 const app = express();
-const db = require("./config/keys");
-
-console.log(db);
+const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
   .then(() => console.log('Connected to MongoDB Successfully'))
   .catch(err => console.log(err));
 
